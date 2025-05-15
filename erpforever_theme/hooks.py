018950f932ac1405bd3286458ba09760fc2e9984
Mojeer Salman <info@iweb.ps>
2025-05-15 17:17:34 +0300

    Create hooks.py

---
+++ erpforever_theme/hooks.py
@@ -0,0 +1,8 @@
+app_name = "erpforever_theme"
+app_title = "ERPForever Theme"
+app_publisher = "Mojeer Salman"
+app_description = "ERPForever Custom theme"
+app_email = "info@iweb.ps"
+app_license = "MIT"
+
+app_include_js = "/assets/erpforever_theme/js/inject_theme.js"
