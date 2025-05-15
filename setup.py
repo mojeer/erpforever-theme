1738e3e40e5cc28c4ca67776cc6675733e781c78
Mojeer Salman <info@iweb.ps>
2025-05-15 17:15:25 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name='erpforever_theme',
+    version='0.0.1',
+    description='ERPForever Custom theme',
+    author='Mojeer Salman',
+    author_email='info@iweb.ps',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+)
