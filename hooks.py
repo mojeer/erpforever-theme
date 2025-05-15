35c3185762ad760120fda335c704b3a3e7d3fd09
Mojeer Salman <info@iweb.ps>
2025-05-15 17:04:59 +0300

    Add files via upload

---
+++ hooks.py
@@ -0,0 +1,8 @@
+app_name = "erpforever_theme"
+app_title = "ERPForever Theme"
+app_publisher = "Your Name"
+app_description = "ERPForever Custom theme"
+app_email = "info@iweb.ps"
+app_license = "MIT"
+
+app_include_js = "/assets/remote_theme/js/inject_theme.js"
