27e0c18f191a6fd3e8b8fd9b5af31bdbda88cb9c
Mojeer Salman <info@iweb.ps>
2025-05-16 13:52:02 +0300

    Delete hooks.py

--- hooks.py
+++
@@ -1,7 +0,0 @@
-app_name = "erpforever_theme"
-app_title = "ERPForever Theme"
-app_publisher = "Mojeer Salman"
-app_description = "ERPForever Custom theme"
-app_email = "info@iweb.ps"
-app_license = "MIT"
-app_include_js = "/assets/remote_theme/js/inject_theme.js"
